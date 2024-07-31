332d1a3cc013d3ad369763bc894a9f67c5dc9085
Jonathas <aluno@ifpe>
2024-07-31 18:08:22 -0300

    31/07/2024

--- src/main/java/br/com/ifpe/oxefood/api/categoriaproduto/CategoriaProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/categoriaproduto/CategoriaProdutoController.java
@@ -1,6 +1,7 @@
 package br.com.ifpe.oxefood.api.categoriaproduto;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,14 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.ifpe.oxefood.api.cliente.ClienteRequest;
 import br.com.ifpe.oxefood.modelo.categoriaproduto.CategoriaProduto;
 import br.com.ifpe.oxefood.modelo.categoriaproduto.CategoriaProdutoService;
-import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
-@RequestMapping("/api/CategoriaProduto")
+@RequestMapping("/api/categoriaproduto")
 @CrossOrigin
 
 public class CategoriaProdutoController {
@@ -29,63 +28,47 @@ public class CategoriaProdutoController {
     @Autowired
     private CategoriaProdutoService categoriaProdutoService;
 
-    @Operation(
-       summary = "Serviço responsável por salvar um cliente no sistema.",
-       description = "Exemplo de descrição de um endpoint responsável por inserir um cliente no sistema."
-       )
-
+    @Operation(summary = "Serviço responsável por salvar um cliente no sistema.", description = "Exemplo de descrição de um endpoint responsável por inserir um cliente no sistema.")
 
     @PostMapping
     public ResponseEntity<CategoriaProduto> save(@RequestBody CategoriaProdutoRequest request) {
 
-        CategoriaProduto categoriaProduto = categoriaProdutoService.save(request.build());
+        CategoriaProduto categoriaProdutoNovo = (request.build());
+        CategoriaProduto categoriaProduto = categoriaProdutoService.save(categoriaProdutoNovo);
         return new ResponseEntity<CategoriaProduto>(categoriaProduto, HttpStatus.CREATED);
     }
 
-    @Operation(
-        summary = "Serviço responsável por listar os clientes.",
-        description = "Exemplo de descrição de um endpoint responsável por inserir um cliente no sistema."
-        )
+    @Operation(summary = "Serviço responsável por listar os clientes.", description = "Exemplo de descrição de um endpoint responsável por inserir um cliente no sistema.")
 
     @GetMapping
     public List<CategoriaProduto> listarTodos() {
         return categoriaProdutoService.listarTodos();
     }
 
-    @Operation(
-        summary = "Serviço responsável por listar os clientes."
-        )
-
+    @Operation(summary = "Serviço responsável por listar os clientes.")
 
     @GetMapping("/{id}")
     public CategoriaProduto obterPorID(@PathVariable Long id) {
         return categoriaProdutoService.obterPorID(id);
     }
 
-    @Operation(
-        summary = "Serviço responsável por alterar as informações dos clientes."
-        )
+    @Operation(summary = "Serviço responsável por alterar as informações dos clientes.")
 
-
-     @PutMapping("/{id}")
-    public ResponseEntity<CategoriaProduto> update(@PathVariable("id") Long id, @RequestBody CategoriaProdutoRequest request) {
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoriaProduto> update(@PathVariable("id") Long id,
+            @RequestBody CategoriaProdutoRequest request) {
 
         categoriaProdutoService.update(id, request.build());
-    return ResponseEntity.ok().build();
- }
+        return ResponseEntity.ok().build();
+    }
 
-    @Operation(
-        summary = "Serviço responsável por deletar os dados do cliente.",
-        description = "Exemplo de descrição de um endpoint responsável por excluir um cliente."
-        )
-
+    @Operation(summary = "Serviço responsável por deletar os dados do cliente.", description = "Exemplo de descrição de um endpoint responsável por excluir um cliente.")
 
     @DeleteMapping("/{id}")
-   public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
 
         categoriaProdutoService.delete(id);
-       return ResponseEntity.ok().build();
-   }
-
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.ifpe.oxefood.api.cliente;
 
 import java.util.List;
+import lombok.Builder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,16 +16,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+import br.com.ifpe.oxefood.modelo.cliente.EnderecoCliente;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/cliente")
 @CrossOrigin
 public class ClienteController {
 
+    @PostMapping("/endereco/{clienteId}")
+   public ResponseEntity<EnderecoCliente> adicionarEnderecoCliente(@PathVariable("clienteId") Long clienteId, @RequestBody @Valid EnderecoClienteRequest request) {
+
+       EnderecoCliente endereco = clienteService.adicionarEnderecoCliente(clienteId, request.build());
+       return new ResponseEntity<EnderecoCliente>(endereco, HttpStatus.CREATED);
+   }
+
+   @PutMapping("/endereco/{enderecoId}")
+   public ResponseEntity<EnderecoCliente> atualizarEnderecoCliente(@PathVariable("enderecoId") Long enderecoId, @RequestBody EnderecoClienteRequest request) {
+
+       EnderecoCliente endereco = clienteService.atualizarEnderecoCliente(enderecoId, request.build());
+       return new ResponseEntity<EnderecoCliente>(endereco, HttpStatus.OK);
+   }
+  
+   @DeleteMapping("/endereco/{enderecoId}")
+   public ResponseEntity<Void> removerEnderecoCliente(@PathVariable("enderecoId") Long enderecoId) {
+
+       clienteService.removerEnderecoCliente(enderecoId);
+       return ResponseEntity.noContent().build();
+   }
+   
    @Autowired
    private ClienteService clienteService;
 
